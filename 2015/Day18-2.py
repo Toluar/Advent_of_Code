76406bcc8e415e483f7e6ed680061e01b0f9378e
Thomas Gaillard <thomas.gaillard-consultant@dgfip.finances.gouv.fr>
2024-12-04 13:46:06 +0100

    2015 Day 18

---
+++ 2015/Day18-2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+
+with open('Input.18') as f:
+    Data = [x.strip() for x in f.readlines()]
+
+STEP = 100
+
+L = len(Data)
+C = len(Data[0])
+D = defaultdict(int)
+for i in range(L):
+    for j in range(C):
+        D[j,i] = 1 if Data[i][j] == '#' else 0
+
+
+def Shift(DA):
+    DR = defaultdict(int)
+    for i in range(L):
+        for j in range(C):
+            NB = DA[i-1,j-1] + DA[i-1,j] + DA[i-1,j+1] + DA[i,j-1] + DA[i,j+1] + DA[i+1,j-1] + DA[i+1,j] + DA[i+1,j+1]
+            if DA[i,j] == 1 :
+                DR[i,j] = 1 if NB == 2 or NB == 3 else 0
+            else :
+                DR[i,j] = 1 if NB == 3 else 0
+    DR[0,0] = 1
+    DR[L-1,C-1] = 1
+    DR[0,C-1] = 1
+    DR[L-1,0] = 1
+    return DR
+
+for _ in range(STEP):
+    D = Shift(D)
+
+print(sum(D.values()))

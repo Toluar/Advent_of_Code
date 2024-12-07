61719316588548776268e3c1421db4a99df404c6
Thomas Gaillard <thomas.gaillard-consultant@dgfip.finances.gouv.fr>
2024-12-07 10:19:32 +0100

    2024 Day 07

---
+++ 2024/Day07-1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+with open("Input.07") as f:
+    Data = f.read().strip().split("\n")
+
+
+def Is_Valid (R,O):
+    if len(O) == 1 :
+        return( O[0] == R)
+    else:
+        TA = [O[0]+O[1]] + O[2::]
+        TB = [O[0]*O[1]] + O[2::]
+        if Is_Valid(R,TA) :
+            return True
+        if Is_Valid(R,TB) :
+            return True
+        return False
+
+
+Somme = 0
+for Line in Data :
+    Resultat,Op = Line.split(':')
+    Resultat = int(Resultat)
+    Operandes = [int(x) for x in Op.split()]
+    if Is_Valid(Resultat,Operandes) :
+        Somme += Resultat
+
+print(Somme)
